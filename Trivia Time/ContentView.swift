79b311e89f82c0f1ebb628c70ee3e29aef69800c
Parker <Parker.engelson09@gmail.com>
2025-11-20 12:29:54 -0800

    Added Title page to instructions view

--- Trivia Time/ContentView.swift
+++ Trivia Time/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var time = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Trivia Time")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
+        NavigationView {
+            
+            VStack {
+                
+                
+                NavigationLink("PLAY", destination: InstructionsView())
+                Picker(selection: .constant(4), label: Text("Time")) {
+                    Text("30").tag(1)
+                    Text("1:00").tag(2)
+                    Text("1:30").tag(3)
+                    Text("2:00").tag(4)
+                    
+                        
+                    
+                }
+            }
+            
+            
         }
-        .padding()
+        
     }
+    
+    
 }
-
 #Preview {
     ContentView()
 }
+
---
+++ Trivia Time/InstructionsAndTimeSelectView.swift
@@ -0,0 +1,15 @@
+//
+//  InstructionsAndTimeSelectView.swift
+//  Trivia Time
+//
+//  Created by Parker Engelson on 11/20/25.
+//
+import SwiftUI
+struct InstructionsView: View {
+    var body: some View {
+        Text("Instructions and Time Select View")
+    }
+}
+#Preview {
+    InstructionsView()
+}
