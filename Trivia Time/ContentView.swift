97ced0026828b4f3aea15b35959876c015d3fc72
Parker <Parker.engelson09@gmail.com>
2025-11-20 09:51:17 -0800

    Initial Commit

---
+++ Trivia Time/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trivia Time
+//
+//  Created by Parker Engelson on 11/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trivia Time/Trivia_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_TimeApp.swift
+//  Trivia Time
+//
+//  Created by Parker Engelson on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
