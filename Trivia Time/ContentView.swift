a7a92ee2390cced3ed03130a5960dc0793df4704
Parker <Parker.engelson09@gmail.com>
2025-11-21 09:06:51 -0800

    Added link to game page, button to access question is temporary

--- Trivia Time/ContentView.swift
+++ Trivia Time/ContentView.swift
@@ -18,26 +18,20 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding()
-                
+                NavigationLink("Play", destination: GameView()) // sends to PlayView
                 NavigationLink("Instructions", destination: InstructionsView()) // button to access instructions
                 Picker(selection: .constant(4), label: Text("Time")) { // time selection still to be updated
                     Text("30").tag(1)
                     Text("1:00").tag(2)
                     Text("1:30").tag(3)
                     Text("2:00").tag(4)
-                    
-                        
-                    
+                        //might delete, considering shifting to a streak based game
                 }
             }
-            
-            
         }
-        
     }
-    
-    
 }
+
 #Preview {
     ContentView()
 }
--- Trivia Time/GameView.swift
+++ Trivia Time/GameView.swift
@@ -1,22 +1,25 @@
 import SwiftUI
 
 struct GameView: View {
+    
     @State private var questions: [String] = [] // question list
+    @State private var currentQuestion: String = "" // question being shown
     
     var body: some View {
         VStack {
-            //Text((questions[1]))
-            
-            Button("Load Questions") { // accesses question list
-                loadQuestions()
+            Button("Load Questions") { // brings new question
+                questions = loadQuestions() // load question onto new array
+                if !questions.isEmpty { // Check to look for empty array
+                    currentQuestion = questions.randomElement() ?? ""
+                }
             }
+            Text(currentQuestion) // question
         }
     }
-    
-    func loadQuestions() { // holds questions
-        questions.append("What is your name?")
-        questions.append("What is your name? 1")
-    }
+}
+
+func loadQuestions() -> [String] { // holds questions
+    return ["yes", "no"]
 }
 
 #Preview {
